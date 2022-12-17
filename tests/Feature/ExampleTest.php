01f99d6ad2a9b9630014e41655f62ae59b16f984
Bunthoeun Tok <buntheoun.code@gmail.com>
2022-12-17 15:35:34 +0700

    remove pest testing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bunthoeuntok\SimplePermission\Tests\Feature;
+
+use Bunthoeuntok\SimplePermission\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_to_be_true()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Bunthoeuntok\SimplePermission\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
